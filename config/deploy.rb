4e22a5cc61fed9492ab76ec904fc2c57b2746643
Eric Syu <xeriok02390@gmail.com>
2019-11-09 15:57:42 +0800

    設定cap

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,8 @@
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.11.1"
 
-set :application, "my_app_name"
-set :repo_url, "git@example.com:me/my_repo.git"
+set :application, "candidate2020"
+set :repo_url, "git@github.com:newpowerparty/candidate2020.git"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
